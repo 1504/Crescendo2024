fc193c63fae413956da3bb27b18c4ef218544d33
xuz24 <zijie.xu06@gmail.com>
2024-01-14 19:12:14 -0500

    add more shuffleboard

--- Crescendo2024_v2/src/main/java/frc/robot/RobotContainer.java
+++ Crescendo2024_v2/src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import frc.robot.Constants.IOConstants;
-import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Tank;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ShuffleboardManager;
@@ -23,7 +22,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Drivetrain m_drive = Drivetrain.getInstance();
-  private final ShuffleboardManager m_ShuffleboardManager = ShuffleboardManager.getInstance();
+  public final ShuffleboardManager m_ShuffleboardManager = ShuffleboardManager.getInstance();
   private final Joystick _joystickOne = new Joystick(IOConstants.JOYSTICK_ONE); //Controller for translation
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
--- Crescendo2024_v2/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Crescendo2024_v2/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -11,10 +11,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
